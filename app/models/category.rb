c56ceea47b02891f3de4c860f478966c9c951d3e
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-06-05 18:44:50 +0200

    Update model associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_and_belongs_to_many :transactions
+  has_and_belongs_to_many :categories_transactions, class_name: 'Transaction'
 end
