bce0ec7f103cc852f029999f8d0814d29effc5b8
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-06-05 17:34:07 +0200

    Add categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,27 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.includes(:transactions).where(author: current_user)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params.merge(author: current_user))
+
+    if @category.save
+      flash[:notice] = 'Category was successfully created.'
+      redirect_to categories_path
+    else
+      flash[:alert] = 'Category was not created.'
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :Icon)
+  end
+end
