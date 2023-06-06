35b9ed5000c3b037402cbae59c42e9f71aca23b7
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-06-06 15:07:18 +0200

    Set up Active Storage

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :categories_transactions, class_name: 'Transaction'
+
+  has_one_attached :icon
 end
